fd1fa091dccfbfd4117f29bfe3e896859594f412
sebmaster <Sebastian.Rink@datev.de>
2017-10-14 11:18:02 +0200

    Adds first raise

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -12,13 +12,13 @@ public class Player {
     public static int betRequest(JsonElement request) {
         try {
             JsonArray asJsonArray = request.getAsJsonObject().getAsJsonArray("Game");
-            System.out.println("Heureka");
+            System.out.println(asJsonArray.getAsString());
             int minimum_raise = asJsonArray.getAsJsonObject().get("minimum_raise").getAsInt();
             System.out.println("Heureka2");
             int current_buyin = asJsonArray.getAsJsonObject().get("current_buy_in").getAsInt();
             System.out.println("Heureka3");
 
-            
+
             return current_buyin + minimum_raise + 1;
 
         }

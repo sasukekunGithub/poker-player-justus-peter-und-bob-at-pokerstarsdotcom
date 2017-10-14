5714daa62fffa1a759f7bfaba81ae594220f2d05
sebmaster <Sebastian.Rink@datev.de>
2017-10-14 10:44:43 +0200

    Adds first bid

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,5 +1,6 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 
 import java.util.Map;
@@ -9,8 +10,11 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        //request.getJSONArray();
-        return 1000;
+        JsonArray asJsonArray = request.getAsJsonObject().getAsJsonArray("Game state");
+        int minimumraise = asJsonArray.getAsJsonObject().get("minimum_raise").getAsInt();
+        int currentbuyin = asJsonArray.getAsJsonObject().get("current_buy_in").getAsInt();
+
+        return currentbuyin+minimumraise+1;
     }
 
     public static void showdown(JsonElement game) {

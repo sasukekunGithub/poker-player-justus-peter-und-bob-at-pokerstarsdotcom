a4ec80de255c6bedac2d69db6da68770922697f3
sebmaster <Sebastian.Rink@datev.de>
2017-10-14 12:59:37 +0200

    Adds first raise

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -12,24 +12,25 @@ public class Player {
 
     public static int betRequest(JsonElement request) {
         try {
-            System.out.print("in bet");
+            System.out.println("in bet");
 
             JsonObject jsonObject = request.getAsJsonObject();
             JsonElement small_blind = jsonObject.get("small_blind");
             JsonElement current_buy_in = jsonObject.get("current_buy_in");
-            System.out.print(small_blind.getAsString());
-
+            System.out.println(small_blind.getAsString());
+            System.err.println("in bet err");
             return 1000;
 
         }
         catch (Exception e)
         {
+            System.err.println("in bet in catch");
             System.out.println(e.getMessage());
             return 1000;
         }
     }
-    public static void showdown(JsonElement game) {
-        System.out.print("in showdown");
+    public static void showdown(JsonElement game){
+        System.out.println("in showdown");
     }
 }
 
